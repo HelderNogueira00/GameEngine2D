932da4f1ebac5716cfb703f1c2dcdd9a8b2252fd
HelderNogueira00 <heldernogueira.dev@gmail.com>
2025-10-07 05:43:29 -0400

    UI Editor Update

--- editorElement.php
+++ editorElement.php
@@ -2,4 +2,28 @@
     <p>Viewing Editor Mode</p>
 
     <button id="play">PLAY</button>
+    <div class="wrapper">
+        
+        <div class="objects-window editor-window">
+            <div class="head">
+                <h2>World Objects</h2>
+            </div>
+            <div class="body"></div>
+        </div>
+    
+        <div class="workplace-window  editor-window">
+            <div class="head">
+                <h2>Workplace Area</h2>
+            </div>
+            <div class="body"></div>
+        </div>
+    
+        <div class="properties-window  editor-window">
+            <div class="head">
+                <h2>Propriedades</h2>
+            </div>
+            <div class="body"></div>
+        </div>
+    
+    </div>
 </div>
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
     <head>
         <title>GameEngine 2D</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/main.css"/>
     </head>
     <body>
